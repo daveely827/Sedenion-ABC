9aa468b37492e0fa121e4bd1987cb57e1e062a32
daveely827 <124839204+daveely827@users.noreply.github.com>
2025-12-18 16:59:36 -0800

    Create lakefile.lean

---
+++ lakefile.lean
@@ -0,0 +1,11 @@
+import Lake
+open Lake DSL
+
+package «sedenion-abc» where
+  -- Settings go here
+
+lean_lib «SedenionABC» where
+  -- Settings go here
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
